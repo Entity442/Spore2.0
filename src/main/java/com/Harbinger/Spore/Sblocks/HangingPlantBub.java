55aa2f9e08c47c4fa18337e2900387dae7e5ead6
Entity442 <mihailungoci87@gmail.com>
2024-01-03 13:34:03 -0800

    u

--- src/main/java/com/Harbinger/Spore/Sblocks/HangingPlantBub.java
+++ src/main/java/com/Harbinger/Spore/Sblocks/HangingPlantBub.java
@@ -15,11 +15,17 @@ import net.minecraft.world.entity.AreaEffectCloud;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.AirBlock;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 
 public class HangingPlantBub extends HangingPlant{
-
+    public static final BooleanProperty PERSISTENT = BlockStateProperties.PERSISTENT;
+    public HangingPlantBub(){
+        this.registerDefaultState(this.stateDefinition.any().setValue(HANGING, Boolean.FALSE).setValue(WATERLOGGED, Boolean.FALSE).setValue(PERSISTENT,Boolean.TRUE));
+    }
 
     @Override
     public void entityInside(BlockState state, Level level, BlockPos blockpos, Entity entity) {
@@ -42,7 +48,7 @@ public class HangingPlantBub extends HangingPlant{
     }
     @Override
     public boolean isRandomlyTicking(BlockState state) {
-        return state.getValue(HANGING);
+        return state.getValue(HANGING) && state.getValue(PERSISTENT);
     }
 
     @Override
@@ -55,9 +61,13 @@ public class HangingPlantBub extends HangingPlant{
             if (blockState.getBlock() instanceof AirBlock && blockState1.getBlock() instanceof AirBlock && blockState2.getBlock() instanceof AirBlock){
                 BlockState block = Sblocks.BLOOM_GG.get().defaultBlockState();
                 level.setBlock(blockpos,Sblocks.HANGING_FUNGAL_STEM.get().defaultBlockState(), 2);
-                level.setBlock(blockpos.below(),block.setValue(HANGING,true),2);
+                level.setBlock(blockpos.below(),block.setValue(HANGING,true).setValue(PERSISTENT, !(Math.random() < 0.2)),2);
             }
         }
+    }
+
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> stateBuilder) {
+        stateBuilder.add(HANGING,WATERLOGGED,PERSISTENT);
     }
 
 }
--- src/main/java/com/Harbinger/Spore/Sblocks/UnderWaterFungusTop.java
+++ src/main/java/com/Harbinger/Spore/Sblocks/UnderWaterFungusTop.java
@@ -13,9 +13,12 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.level.material.Material;
@@ -24,9 +27,10 @@ import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class UnderWaterFungusTop extends GenericFoliageBlock{
+    public static final BooleanProperty PERSISTENT = BlockStateProperties.PERSISTENT;
     public UnderWaterFungusTop() {
         super(BlockBehaviour.Properties.of(Material.PLANT).strength(0f, 0f).noCollission().noOcclusion().sound(SoundType.CROP).randomTicks());
-        this.registerDefaultState(this.stateDefinition.any().setValue(WATERLOGGED, Boolean.TRUE));
+        this.registerDefaultState(this.stateDefinition.any().setValue(WATERLOGGED, Boolean.TRUE).setValue(PERSISTENT, Boolean.TRUE));
     }
     @Override
     public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
@@ -67,8 +71,17 @@ public class UnderWaterFungusTop extends GenericFoliageBlock{
             BlockState blockState = level.getBlockState(pos.above());
             if (blockState.getFluidState().is(Fluids.WATER)){
                 level.setBlock(pos,Sblocks.UNDERWATER_FUNGAL_STEM.get().defaultBlockState(),3);
-                level.setBlock(pos.above(),Sblocks.UNDERWATER_FUNGAL_STEM_TOP.get().defaultBlockState(),3);
+                BlockState block = Sblocks.UNDERWATER_FUNGAL_STEM_TOP.get().defaultBlockState();
+                level.setBlock(pos.above(),block.setValue(PERSISTENT,!(Math.random() < 0.3)),3);
             }
 
+    }
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> stateBuilder) {
+        stateBuilder.add(WATERLOGGED,PERSISTENT);
+    }
+
+    @Override
+    public boolean isRandomlyTicking(BlockState state) {
+        return state.getValue(PERSISTENT) && state.getValue(WATERLOGGED);
     }
 }
