3f67ef30acb583eb278a24ba2ac1137a5015e3c5
Entity442 <mihailungoci87@gmail.com>
2024-01-24 20:25:00 -0800

    u

--- src/main/java/com/Harbinger/Spore/Client/Renderers/InfectedPlayerRenderer.java
+++ src/main/java/com/Harbinger/Spore/Client/Renderers/InfectedPlayerRenderer.java
@@ -5,11 +5,7 @@ import com.Harbinger.Spore.Sentities.BasicInfected.InfectedPlayer;
 import com.Harbinger.Spore.Spore;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.geom.ModelLayers;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.client.renderer.entity.MobRenderer;
-import net.minecraft.client.renderer.entity.RenderLayerParent;
-import net.minecraft.client.renderer.entity.layers.EyesLayer;
 import net.minecraft.client.renderer.entity.layers.HumanoidArmorLayer;
 import net.minecraft.client.renderer.entity.layers.ItemInHandLayer;
 import net.minecraft.resources.ResourceLocation;
@@ -17,27 +13,19 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
-public class InfectedPlayerRenderer<Type extends InfectedPlayer> extends MobRenderer<Type , InfectedPlayerModel<Type>> {
+public class InfectedPlayerRenderer<Type extends InfectedPlayer> extends BaseInfectedRenderer<Type , InfectedPlayerModel<Type>> {
     private static final ResourceLocation TEXTURE = new ResourceLocation(Spore.MODID,
             "textures/entity/inf_player.png");
+    private static final ResourceLocation EYES_TEXTURE = new ResourceLocation(Spore.MODID,
+            "textures/entity/eyes/inf_player.png");
 
 
     public InfectedPlayerRenderer(EntityRendererProvider.Context context) {
         super(context, new InfectedPlayerModel<>(context.bakeLayer(InfectedPlayerModel.LAYER_LOCATION)), 0.5f);
+
         this.addLayer(new HumanoidArmorLayer(this, new HumanoidModel(context.bakeLayer(ModelLayers.PLAYER_INNER_ARMOR)),
                 new HumanoidModel(context.bakeLayer(ModelLayers.PLAYER_OUTER_ARMOR))));
         this.addLayer(new ItemInHandLayer<>(this, context.getItemInHandRenderer()));
-        this.addLayer(new Eyes<>(this));
-    }
-
-    private static class Eyes<Type extends InfectedPlayer,M extends InfectedPlayerModel<Type>> extends EyesLayer<Type,M>{
-        private static final RenderType EYES = RenderType.eyes(new ResourceLocation(Spore.MODID,"textures/entity/eyes/inf_player.png"));
-        public Eyes(RenderLayerParent layer) {
-            super(layer);
-        }
-        public RenderType renderType() {
-            return EYES;
-        }
     }
 
 
@@ -48,7 +36,7 @@ public class InfectedPlayerRenderer<Type extends InfectedPlayer> extends MobRend
 
 
     @Override
-    protected boolean isShaking(Type type) {
-        return type.isFreazing();
+    public ResourceLocation eyeLayerTexture() {
+        return EYES_TEXTURE;
     }
 }
