1985079d558c40762a9e0928b16cb4b11d4e521a
Entity_Key_Whisper <mihailungoci87@gmail.com>
2024-04-01 00:12:56 -0700

    marker

--- src/main/java/com/Harbinger/Spore/ExtremelySusThings/StructureModification.java
+++ src/main/java/com/Harbinger/Spore/ExtremelySusThings/StructureModification.java
@@ -1,10 +1,14 @@
 package com.Harbinger.Spore.ExtremelySusThings;
 
+import com.Harbinger.Spore.Core.SConfig;
 import com.Harbinger.Spore.Core.Sentities;
 import com.Harbinger.Spore.Spore;
 import com.mojang.serialization.Codec;
 import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.level.biome.MobSpawnSettings;
 import net.minecraft.world.level.levelgen.structure.BuiltinStructures;
@@ -28,9 +32,17 @@ public class StructureModification implements StructureModifier {
         if (structure.is(BuiltinStructures.PILLAGER_OUTPOST)){
             builder.getStructureSettings().getOrAddSpawnOverrides(MobCategory.MONSTER).addSpawn(new MobSpawnSettings.SpawnerData(Sentities.INF_PILLAGER.get(),60,2,4));
         }
-
-        if (structure.is(BuiltinStructures.OCEAN_RUIN_WARM)){
-            builder.getStructureSettings().getOrAddSpawnOverrides(MobCategory.MONSTER).addSpawn(new MobSpawnSettings.SpawnerData(Sentities.INF_DROWNED.get(),30,2,4));
+        if (structure.is(TagKey.create(Registry.STRUCTURE_REGISTRY,new ResourceLocation(Spore.MODID,"laboratories")))){
+            if (!SConfig.SERVER.structure_spawns.get().isEmpty()){
+                for (String str : SConfig.SERVER.structure_spawns.get()) {
+                    String[] string = str.split("\\|");
+                    EntityType<?> entity = ForgeRegistries.ENTITY_TYPES.getValue(new ResourceLocation(string[0]));
+                    if (entity != null){
+                        builder.getStructureSettings().getOrAddSpawnOverrides(entity.getCategory()).addSpawn(new MobSpawnSettings.SpawnerData(entity, Integer.parseUnsignedInt(string[1]),
+                                Integer.parseUnsignedInt(string[2]), Integer.parseUnsignedInt(string[3])));
+                    }
+                }
+            }
         }
     }
 
