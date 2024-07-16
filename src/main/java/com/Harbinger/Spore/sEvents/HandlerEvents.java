75e2601dc8d4052e9d0be53071ebfedb38b43517
Harby <mihailungoci87@gmail.com>
2024-07-16 04:24:02 -0700

    gast

--- src/main/java/com/Harbinger/Spore/sEvents/HandlerEvents.java
+++ src/main/java/com/Harbinger/Spore/sEvents/HandlerEvents.java
@@ -157,10 +157,13 @@ public class HandlerEvents {
                         for (Entity entity1 : entities) {
                             if(entity1 instanceof Infected infected) {
                                 infected.setEvolution(SConfig.SERVER.evolution_age_human.get());
-                                if (infected instanceof EvolvedInfected evolvedInfected){
+                                if (entity1 instanceof Scamper scamper){
+                                    scamper.setAge(SConfig.SERVER.scamper_age.get());
+                                }else if (infected instanceof EvolvedInfected evolvedInfected){
                                     evolvedInfected.setEvoPoints(SConfig.SERVER.min_kills_hyper.get());
-                                }else
-                                infected.setEvoPoints(SConfig.SERVER.min_kills.get());
+                                }else {
+                                    infected.setEvoPoints(SConfig.SERVER.min_kills.get());
+                                }
                             }else if (entity1 instanceof Mound mound){
                                 mound.setAge(mound.getAge()+1);
                             }else if (entity1 instanceof Calamity calamity){
