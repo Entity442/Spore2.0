9c457566c47aaa58f18ffc2a393e9c98abf563ed
Harby <mihailungoci87@gmail.com>
2024-08-07 06:42:48 -0700

    e

--- src/main/java/com/Harbinger/Spore/Sentities/Calamities/Howitzer.java
+++ src/main/java/com/Harbinger/Spore/Sentities/Calamities/Howitzer.java
@@ -99,12 +99,6 @@ public class Howitzer extends Calamity implements TrueCalamity, RangedAttackMob 
     @Override
     public void registerGoals() {
         super.registerGoals();
-        this.goalSelector.addGoal(2, new ScatterShotRangedGoal(this,1,80,64,1,3){
-            @Override
-            public boolean canUse() {
-                return !Howitzer.this.isInMeleeRange() && super.canUse();
-            }
-        });
         this.goalSelector.addGoal(3,new LeapGoal(this,0.9f){
             @Override
             public boolean canUse() {
@@ -124,7 +118,13 @@ public class Howitzer extends Calamity implements TrueCalamity, RangedAttackMob 
             @Override
             protected double getAttackReachSqr(LivingEntity entity) {
                 float f = Howitzer.this.getBbWidth();
-                return (double)(f * f * 1.75F + entity.getBbWidth());
+                return (double)(f * 1.75F * f * 1.75F + entity.getBbWidth());
+            }
+        });
+        this.goalSelector.addGoal(4, new ScatterShotRangedGoal(this,1,80,64,1,3){
+            @Override
+            public boolean canUse() {
+                return !Howitzer.this.isInMeleeRange() && super.canUse();
             }
         });
         this.goalSelector.addGoal(5, new WaterAvoidingRandomStrollGoal(this, 1.2));
