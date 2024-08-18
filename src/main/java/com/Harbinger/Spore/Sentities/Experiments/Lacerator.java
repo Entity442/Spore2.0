078433f2a3d45f0f3df292ee69865f2e12247295
Harby <mihailungoci87@gmail.com>
2024-08-18 02:53:28 -0700

    lacerator

---
+++ src/main/java/com/Harbinger/Spore/Sentities/Experiments/Lacerator.java
@@ -0,0 +1,120 @@
+package com.Harbinger.Spore.Sentities.Experiments;
+
+import com.Harbinger.Spore.Core.SConfig;
+import com.Harbinger.Spore.Core.Ssounds;
+import com.Harbinger.Spore.Sentities.AI.CustomMeleeAttackGoal;
+import com.Harbinger.Spore.Sentities.BaseEntities.Experiment;
+import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.LeapAtTargetGoal;
+import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
+import net.minecraft.world.entity.ai.goal.RandomStrollGoal;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+
+import java.util.List;
+
+public class Lacerator extends Experiment {
+    public Lacerator(EntityType<? extends Monster> type, Level level) {
+        super(type, level);
+    }
+
+    public static AttributeSupplier.Builder createAttributes() {
+        return Mob.createMobAttributes()
+                .add(Attributes.MAX_HEALTH, SConfig.SERVER.lacerator_hp.get() * SConfig.SERVER.global_health.get())
+                .add(Attributes.MOVEMENT_SPEED, 0.3)
+                .add(Attributes.ATTACK_DAMAGE, SConfig.SERVER.lacerator_damage.get() * SConfig.SERVER.global_damage.get())
+                .add(Attributes.ARMOR, SConfig.SERVER.lacerator_armor.get() * SConfig.SERVER.global_armor.get())
+                .add(Attributes.FOLLOW_RANGE, 30)
+                .add(Attributes.ATTACK_KNOCKBACK, 1);
+    }
+
+    @Override
+    public List<? extends String> getDropList() {
+        return SConfig.DATAGEN.lacerator_loot.get();
+    }
+
+    @Override
+    protected void addRegularGoals() {
+        super.addRegularGoals();
+        this.goalSelector.addGoal(3, new LaceratorMeleeAttackGoal(this, 1.2, false) {
+            @Override
+            protected double getAttackReachSqr(LivingEntity entity) {
+                return 2.0 + entity.getBbWidth() * entity.getBbWidth();}});
+        this.goalSelector.addGoal(4, new LeapAtTargetGoal(this,0.2F));
+        this.goalSelector.addGoal(4, new RandomStrollGoal(this, 0.8));
+        this.goalSelector.addGoal(5, new RandomLookAroundGoal(this));
+    }
+
+    protected SoundEvent getAmbientSound() {
+        return Ssounds.INF_GROWL.get();
+    }
+
+    protected SoundEvent getHurtSound(DamageSource p_34327_) {
+        return Ssounds.INF_DAMAGE.get();
+    }
+
+    protected SoundEvent getDeathSound() {
+        return Ssounds.INF_DAMAGE.get();
+    }
+
+    protected SoundEvent getStepSound() {
+        return SoundEvents.ZOMBIE_STEP;
+    }
+
+    protected void playStepSound(BlockPos p_34316_, BlockState p_34317_) {
+        this.playSound(this.getStepSound(), 0.15F, 1.0F);
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        if (tickCount % 40 == 0){
+            this.playSound(Ssounds.ENGINE.get());
+            if (this.isAggressive()){
+                this.playSound(Ssounds.SAW_SOUND.get());
+            }
+        }
+    }
+
+    @Override
+    public boolean doHurtTarget(Entity entity) {
+        if (entity instanceof LivingEntity livingEntity && Math.random() < 0.1f){
+            livingEntity.addEffect(new MobEffectInstance(MobEffects.WITHER,200,0));
+        }
+        return super.doHurtTarget(entity);
+    }
+
+    public static class LaceratorMeleeAttackGoal extends CustomMeleeAttackGoal{
+
+        public LaceratorMeleeAttackGoal(PathfinderMob p_25552_, double p_25553_, boolean p_25554_) {
+            super(p_25552_, p_25553_, p_25554_);
+        }
+
+        @Override
+        protected void resetAttackCooldown(int value) {
+            super.resetAttackCooldown(5);
+        }
+
+        @Override
+        protected void checkAndPerformAttack(LivingEntity entity, double at) {
+            super.checkAndPerformAttack(entity, at);
+            entity.hurtTime = 5;
+            entity.invulnerableTime = 5;
+        }
+
+        @Override
+        public void start() {
+            super.start();
+            this.mob.playSound(Ssounds.SAW_SOUND.get());
+        }
+    }
+}
