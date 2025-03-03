20225a9efc12c0cf89038ab0a9165480476a7839
mihai <mihailungoci87@gmail.com>
2025-03-03 04:14:10 -0800

    cooking

--- src/main/java/com/Harbinger/Spore/Sitems/OrganItem.java
+++ src/main/java/com/Harbinger/Spore/Sitems/OrganItem.java
@@ -8,12 +8,13 @@ import com.Harbinger.Spore.ExtremelySusThings.SporePacketHandler;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -34,24 +35,20 @@ public class OrganItem extends Item {
     }
 
     @Override
-    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> list, TooltipFlag tooltipFlag) {
+    public void appendHoverText(@NotNull ItemStack stack, @Nullable Level level, @NotNull List<Component> list, TooltipFlag tooltipFlag) {
         super.appendHoverText(stack, level, list, tooltipFlag);
-        if (level == null || !level.isClientSide) {
-            return;
-        }
-        Player player = Minecraft.getInstance().player;
-        if (player == null) {
-            return;
-        }
-        MinecraftServer server = Minecraft.getInstance().getSingleplayerServer();
-        if (server == null) {
-            return;
-        }
-        if (ClientAdvancementTracker.hasAdvancement(advancementIds)) {
-            list.add(Component.translatable(info).withStyle(ChatFormatting.GOLD));
-        } else {
+        if (level != null && level.isClientSide) {
+            Entity entity = Minecraft.getInstance().getCameraEntity();
+            if (entity instanceof Player player) {
+                if (ClientAdvancementTracker.hasAdvancement(advancementIds)) {
+                    list.add(Component.translatable(info).withStyle(ChatFormatting.GOLD));
+                } else {
+                    list.add(Component.translatable("spore.scanner.organ.default").withStyle(ChatFormatting.RED));
+                    SporePacketHandler.sendToServer(new RequestAdvancementPacket(advancementIds,player.getId()));
+                }
+            }
+        }else{
             list.add(Component.translatable("spore.scanner.organ.default").withStyle(ChatFormatting.RED));
         }
-        SporePacketHandler.sendToServer(new RequestAdvancementPacket(advancementIds));
     }
 }
