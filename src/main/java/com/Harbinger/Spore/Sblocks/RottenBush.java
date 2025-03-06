7c4e795c0f4ff9b2a60e4d2bf03f85f721fac667
mihai <mihailungoci87@gmail.com>
2025-03-06 10:00:38 -0800

    cooking

---
+++ src/main/java/com/Harbinger/Spore/Sblocks/RottenBush.java
@@ -0,0 +1,43 @@
+package com.Harbinger.Spore.Sblocks;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.material.Material;
+
+public class RottenBush extends GenericFoliageBlock{
+    public static final int MAX_AGE = 5;
+    public static final IntegerProperty AGE = BlockStateProperties.AGE_5;
+    public RottenBush() {
+        super(BlockBehaviour.Properties.of(Material.WOOD).sound(SoundType.CROP).strength(0f, 0f).noCollission().noOcclusion().sound(SoundType.CROP).randomTicks());
+        registerDefaultState(this.stateDefinition.any().setValue(AGE, 0).setValue(WATERLOGGED,false));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> stateBuilder) {
+        super.createBlockStateDefinition(stateBuilder);
+        stateBuilder.add(AGE);
+    }
+
+    @Override
+    public boolean isRandomlyTicking(BlockState state) {
+        return super.isRandomlyTicking(state) || state.getValue(AGE) < MAX_AGE;
+    }
+    public BlockState getStateForAge(int va) {
+        return this.defaultBlockState().setValue(AGE, va);
+    }
+    @Override
+    public void randomTick(BlockState state, ServerLevel serverLevel, BlockPos pos, RandomSource randomSource) {
+        super.randomTick(state, serverLevel, pos, randomSource);
+        if (state.getValue(AGE) < MAX_AGE && Math.random() < 0.3){
+            serverLevel.setBlock(pos, getStateForAge(state.getValue(AGE)+1), 2);
+        }
+    }
+}
