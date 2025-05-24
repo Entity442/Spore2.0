593684382342e5e212299ccf87b724684e32fe2c
mihai <mihailungoci87@gmail.com>
2025-05-24 08:01:41 -0700

    more

--- src/main/java/com/Harbinger/Spore/Sentities/Projectile/ThrownSickle.java
+++ src/main/java/com/Harbinger/Spore/Sentities/Projectile/ThrownSickle.java
@@ -14,6 +14,7 @@ import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.entity.projectile.ThrownTrident;
 import net.minecraft.world.item.ItemStack;
@@ -72,7 +73,10 @@ public class ThrownSickle extends AbstractArrow {
         super.tick();
     }
 
-
+    @Override
+    protected boolean tryPickup(Player p_150121_) {
+        return false;
+    }
     protected ItemStack getPickupItem() {
         return ItemStack.EMPTY;
     }
