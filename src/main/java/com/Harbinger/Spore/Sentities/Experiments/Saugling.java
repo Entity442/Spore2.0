a54b42904d8a38acd6901f57c03c6f354e4a5967
mihai <mihailungoci87@gmail.com>
2025-07-04 10:19:44 -0700

    new sounds fuck me

--- src/main/java/com/Harbinger/Spore/Sentities/Experiments/Saugling.java
+++ src/main/java/com/Harbinger/Spore/Sentities/Experiments/Saugling.java
@@ -156,6 +156,9 @@ public class Saugling extends Experiment {
         }
         return BlockPos.ZERO;
     }
+    public boolean checkChest(Level level){
+        return level.getBlockState(getChestPos()).is(Blocks.CHEST) && getChestPos() != BlockPos.ZERO;
+    }
     @Override
     public void aiStep() {
         super.aiStep();
@@ -166,6 +169,10 @@ public class Saugling extends Experiment {
             --setTicksOut;
         }
         if (isHidden() && tickCount % 20 == 0) {
+            if (!checkChest(level)){
+                setIsHidden(false);
+                setPrimed(false);
+            }
             if (!isPrimed()) {
                 AABB aabb = this.getBoundingBox().inflate(3);
                 List<LivingEntity> livingEntities = this.level.getEntitiesOfClass(LivingEntity.class, aabb,
